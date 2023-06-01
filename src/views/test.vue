939756914aaf1d14fc5f3da092eb0cde5f975f54
laiyao <laiyao050@chinasoftinc.com>
2023-06-01 18:16:29 +0800

    添加test 3d测试

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,90 +1,112 @@
 <template>
-  <div class="bg-white">
-    <header class="absolute inset-x-0 top-0 z-50">
-      <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div class="flex lg:flex-1">
-          <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-          </a>
-        </div>
-        <div class="flex lg:hidden">
-          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon class="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div class="hidden lg:flex lg:gap-x-12">
-          <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</a>
-        </div>
-        <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
-        </div>
-      </nav>
-      <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
-        <div class="fixed inset-0 z-50" />
-        <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div class="flex items-center justify-between">
-            <a href="#" class="-m-1.5 p-1.5">
-              <span class="sr-only">Your Company</span>
-              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-            </a>
-            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
-              <span class="sr-only">Close menu</span>
-              <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div class="mt-6 flow-root">
-            <div class="-my-6 divide-y divide-gray-500/10">
-              <div class="space-y-2 py-6">
-                <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
-              </div>
-              <div class="py-6">
-                <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
-              </div>
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog>
-    </header>
-
-    <div class="relative isolate px-6 pt-14 lg:px-8">
-      <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-        <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
-      </div>
-      <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            Announcing our next round of funding. <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true" />Read more <span aria-hidden="true">&rarr;</span></a>
-          </div>
-        </div>
-        <div class="text-center">
-          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
-          <div class="mt-10 flex items-center justify-center gap-x-6">
-            <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
-          </div>
-        </div>
-      </div>
-      <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-        <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
-      </div>
-    </div>
-  </div>
+  <div class="w-full h-full bg-black/50" ref="viewerRef"></div>
 </template>
+<script lang='ts' setup>
+import { ref, onMounted, Ref } from 'vue';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GUI } from 'dat.gui';
 
-<script setup >
-import { ref } from 'vue'
-import { Dialog, DialogPanel } from '@headlessui/vue'
-import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+const { innerWidth, innerHeight } = window;
+console.log(innerWidth, innerHeight);
 
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
+const aspect = innerWidth / innerHeight
+const camera = new THREE.PerspectiveCamera(75, aspect, 1, 1000);
+camera.position.set( 0, 0, 500 );
+const scene = new THREE.Scene()
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
+renderer.setSize(innerWidth, innerHeight);
 
-const mobileMenuOpen = ref(false)
+const geometry = new THREE.BoxGeometry( 100, 100, 100 );
+const material = new THREE.MeshLambertMaterial( { color: 0x00ff00, side: THREE.DoubleSide } );
+const cube = new THREE.Mesh( geometry, material );
+
+cube.position.set(0, 0, 0);
+scene.add( cube );
+
+
+const line = new THREE.Line(geometry, material);
+line.position.set(200, 0, 0);
+scene.add( line );
+
+const line2 = new THREE.Line(geometry, material);
+line2.position.set(100, 0, 0);
+scene.add( line2 );
+
+
+console.log(camera);
+console.log(scene);
+const viewerRef: Ref<HTMLDivElement | undefined> = ref<HTMLDivElement>();
+
+// 辅助观察坐标系
+const axesHelper = new THREE.AxesHelper(300);
+scene.add(axesHelper);
+
+// 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', function () {
+  renderer.render(scene, camera); //执行渲染操作
+});//监听鼠标、键盘事件
+
+
+// 添加点光源
+// const sportLight = new THREE.SpotLight(0xffffff, 1.0, 200);
+// sportLight.position.set(200, 200, 100);
+
+// let helper = new THREE.SpotLightHelper(sportLight);
+// scene.add(helper);
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2); // soft white light
+scene.add( ambientLight );
+
+// let ambientLightHelper = new THREE.AmbientLightHelper(ambientLight);
+// scene.add(ambientLightHelper);
+
+const geometry02= new THREE.PlaneGeometry( 100, 100, 100, 100 );
+const material02 = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+const plane = new THREE.Mesh( geometry02, material02 );
+
+plane.position.set(200, 0, 0)
+scene.add( plane );
+
+// onresize 事件会在窗口被调整大小时发生
+window.onresize = function () {
+  // 重置渲染器输出画布canvas尺寸
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // 全屏情况下：设置观察范围长宽比aspect为窗口宽高比
+  camera.aspect = window.innerWidth / window.innerHeight;
+  // 渲染器执行render方法的时候会读取相机对象的投影矩阵属性projectionMatrix
+  // 但是不会每渲染一帧，就通过相机的属性计算投影矩阵(节约计算资源)
+  // 如果相机的一些属性发生了变化，需要执行updateProjectionMatrix ()方法更新相机的投影矩阵
+  camera.updateProjectionMatrix();
+};
+
+
+
+const gui = new GUI();
+// gui.add(ambientLight, 'intensity', 0, 2.0);
+gui.add(cube.position, 'x', 0, 180);
+gui.add(cube.position, 'y', 0, 180);
+gui.add(cube.position, 'z', 0, 180);
+
+onMounted(() => {
+  console.log(viewerRef.value);
+  (viewerRef.value as HTMLDivElement).appendChild(renderer.domElement);
+});
+
+(function animate() {
+  requestAnimationFrame( animate );
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
+  renderer.render( scene, camera );
+})();
+
+
+
 </script>
+<style>
+ 
+</style>
